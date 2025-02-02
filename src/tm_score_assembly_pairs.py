4029d77fed251914256e0d8bceb7b35a10637f77
Joan Segura <jsegurmor@gmail.com>
2025-02-02 11:19:55 -0800

    us-align assembly list

--- src/tm_score_assembly_pairs.py
+++ src/tm_score_assembly_pairs.py
@@ -1,11 +1,24 @@
 import csv
+import argparse
 
 from utils.parse_tm_align import parse_tm_align
 from utils.run_command import run_command
 
 US_ALIGN_BIN  = "/home/jseguramora/devel/rcsb-embedding-training-generator/resources/us-align/USalign"
 if __name__ == "__main__":
-    with open('data.csv', 'r') as file:
+    parser = argparse.ArgumentParser(
+        description="Use compute_tm_score_list \
+        to calculate TM-score between structure chains of a query and target lists",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--list_file",
+        type=str,
+        required=True,
+        help="List of PDB chains e.g. 101M.A",
+    )
+    cfg = parser.parse_args()
+    with open(cfg.list_file, 'r') as file:
         csv_reader = csv.reader(file)
         for (pdb_i, pdb_j, score) in csv_reader:
             cmd = f"{US_ALIGN_BIN} -mm 1 -ter 0 {pdb_i}.pdb {pdb_j}.pdb"
